ad305dcf0be2554ad9e372904f88d9e48afff74e
Lindokuhle Mtengwane <lindokuhlemtengwane@gmail.com>
2024-05-24 01:19:23 +0200

    used forEach, map, filter, sort and reduce

--- StudentNo_Classcode_Group_Name-Surname_DJS06-main/scripts.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS06-main/scripts.js
@@ -33,3 +33,36 @@ const products = [
 names.forEach((name, index) => {
   console.log(`${name} (${provinces[index]})`);
 });
+
+// to create a new array of province names in all uppercase using map:
+
+const uppercaseProvinces = provinces.map((province) => province.toUpperCase());
+console.log(uppercaseProvinces);
+
+// create name length array using map:
+
+const nameLengths = names.map((name) => name.length);
+console.log(nameLengths);
+
+// sort provinces alphabetically using sort:
+
+const sortedProvinces = provinces.sort();
+console.log(sortedProvinces);
+
+// filter out provinces containing "Cape" using filter:
+
+const CapeProvinces = provinces.filter((province) => province.includes("Cape"));
+console.log(CapeProvinces);
+
+// create boolean array using map and some:
+
+const containsS = names.map((name) => name.includes("S"));
+console.log(containsS);
+
+// use reduce to transform the names array into an object mapping names to their respective provinces:
+
+const nameToProvinceMap = names.reduce((acc, name, index) => {
+  acc[name] = provinces[index];
+  return acc;
+}, {});
+console.log(nameToProvinceMap);
